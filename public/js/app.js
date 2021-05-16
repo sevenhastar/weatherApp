d4930ef859e3ce9a714fad3a23b46aead3acaed5
Seven Hastar <sevenhastar@Sevens-MacBook-Pro.local>
2021-05-16 21:33:34 +0530

    Adding the hostname

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e)=>{
     console.log(search.value)
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ''
-    fetch('http://weatherapp-env.eba-vymg37g7.us-east-1.elasticbeanstalk.com//weather?address='+search.value).then((response)=>{
+    fetch('http://weatherapp-env.eba-vymg37g7.us-east-1.elasticbeanstalk.com/weather?address='+search.value).then((response)=>{
         console.log(response)
     response.json().then((data)=>{
         if(data.error){
